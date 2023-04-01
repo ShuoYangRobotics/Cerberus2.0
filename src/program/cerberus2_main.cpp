4017230a67f7d4862f652fce1ce8cf33488a448a
shuo <shuoyang@andrew.cmu.edu>
2023-04-01 17:56:00 -0400

    add basic skeleton

---
+++ src/program/cerberus2_main.cpp
@@ -0,0 +1,28 @@
+// standard C
+#include <signal.h>
+#include <thread>
+
+// ros related
+#include <geometry_msgs/PoseStamped.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+
+// Define the function to be called when ctrl-c (SIGINT) is sent to process
+void signal_callback_handler(int signum) {
+  std::cout << "Caught signal " << signum << std::endl;
+  // Terminate program
+  exit(signum);
+}
+
+int main(int argc, char** argv) {
+  // Register signal and signal handler
+  signal(SIGINT, signal_callback_handler);
+  ros::init(argc, argv, "cerberus2");
+  ros::NodeHandle nh;
+  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+
+  /* create an estimator object */
+
+  ros::spin();
+  return 0;
+}
